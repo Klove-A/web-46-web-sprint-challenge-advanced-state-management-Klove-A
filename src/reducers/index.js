738ea7d15fbf81ab680de90d2a2848e9dbbde12f
Klove Adams <kalebsxott@gmail.com>
2021-09-10 15:50:01 -0400

    built reducer and added cases

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,31 @@ export const initialState = {
   error: ""
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action)=> {
+  switch (action.type) {
+    case(SMURF_FETCH):
+      return({
+        ...state, isLoading: true, error: ""
+      });
+    case(FETCH_SUCCESS):
+      return({
+        ...state, smurfs: action.payload, isLoading: false
+      });
+    case(FETCH_FAIL):
+      return({
+        ...state, error: action.payload, isLoading: false
+      });
+    case(ADD_SMURF):
+      return ({
+        ...state, smurfs: [...state.smurfs, action.payload]
+      });
+    case(ERROR_CASE):
+      return({
+        ...state, error: action.payload
+      })
+    default:
+      return state;
+  }
 }
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
